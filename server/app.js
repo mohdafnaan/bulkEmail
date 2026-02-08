7c684dfc56d82679ea0c8aefac32b28a525ba043
mohdafnaan <mohdafnaan833@gmail.com>
2026-02-08 06:33:58 +0530

    dist added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import cors from "cors";
+// import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const buildPath = path.join(__dirname, "dist");
 // import database
 import "./utils/dbConnect.js";
 // import public routers
@@ -21,24 +20,25 @@ const app = express();
 app.use(express.json());
 app.use(express.static(buildPath));
 const port = process.env.PORT;
+const buildPath = path.join(__dirname, "dist");
 
-let corsObject = {
-  origin: ["http://localhost:5173","https://cvoploader.afnaan.in"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
+// let corsObject = {
+//   origin: ["http://localhost:5173","https://cvuploader.afnaan.in"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
 
 app.use(cors(corsObject));
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
-app.get("/", (req, res) => {
-  try {
-    res.status(200).json({ msg: "server is live" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-});
+// app.get("/", (req, res) => {
+//   try {
+//     res.status(200).json({ msg: "server is live" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error);
+//   }
+// });
 app.use("/public", userRouter);
 app.use(middleware);
 app.use("/private", privateUserRouter);
